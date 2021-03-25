ad1b8abefeae3989a35a4ec03830f2e3f219023a
106306047 <linoliver97@gmail.com>
2021-03-25 20:11:25 +0800

    boardTest

--- calculator0325/calculator0325/ViewController.swift
+++ calculator0325/calculator0325/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var board: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func num0(_ sender: Any) {
+    }
+    @IBAction func num1(_ sender: Any) {
+//        guard !(board.text!.isEmpty) else {
+//            return
+//        }
+//        let text = textField.text!
+//        textlabel.text = "I'm, \(text)"
+    }
+    
+    @IBAction func num2(_ sender: Any) {
+    }
+    @IBAction func num3(_ sender: Any) {
+    }
+    @IBAction func num4(_ sender: Any) {
+    }
+    @IBAction func num5(_ sender: Any) {
+    }
+    @IBAction func num6(_ sender: Any) {
+    }
+    @IBAction func num7(_ sender: Any) {
+    }
+    @IBAction func num8(_ sender: Any) {
+    }
+    @IBAction func num9(_ sender: Any) {
+    }
+    
+    
+    
 
 }
 
